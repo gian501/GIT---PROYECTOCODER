38de66a0f8295d3b28853867b774ba15a8e82030
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-09 12:30:29 -0300

    Punto seguro, models listos

---
+++ AppCoder/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+def curso(self):
+    curso = Curso(nombre = "Desarrollo Web", comision = 19881)
+    curso
+    documentoDeTexto = f"--> Curso:{curso.nombre} comision:{curso.comision}"
+    return HttpResponse(documentoDeTexto)
